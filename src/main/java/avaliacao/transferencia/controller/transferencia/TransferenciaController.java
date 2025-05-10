b7bae154fa667353d4fe9981c3db219e997fa648
Gabriel Aguiar <aguiarogabriel@hotmail.com>
2025-05-09 23:04:20 -0300

    feature (TransferenciaController): implementando JavaDoc

--- src/main/java/avaliacao/transferencia/controller/transferencia/TransferenciaController.java
+++ src/main/java/avaliacao/transferencia/controller/transferencia/TransferenciaController.java
@@ -9,30 +9,52 @@ import avaliacao.transferencia.service.transferencia.TransferenciaService;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * Controller REST responsável por gerenciar as operações relacionadas às transferências financeiras.
+ * Disponibiliza endpoints para realizar transferências, consultar agendamentos e transferências efetuadas.
+ *
+ * @author Gabriel Aguiar
+ */
 @RestController
 @RequestMapping("/transferencia")
 public class TransferenciaController {
+
     @Autowired
     TransferenciaService transferenciaService;
 
-    Logger logger = Logger.getLogger(TransferenciaController.class.getName());
+    private final Logger logger = Logger.getLogger(TransferenciaController.class.getName());
 
+    /**
+     * Endpoint para realizar uma nova transferência financeira.
+     *
+     * @param transferenciaEntity Entidade contendo os dados da transferência a ser realizada.
+     * @return ResponseEntity contendo a entidade da transferência realizada.
+     */
     @PostMapping("/transferir")
-    public ResponseEntity<TransferenciaEntity> transferir(@RequestBody TransferenciaEntity transferenciaEntity){
+    public ResponseEntity<TransferenciaEntity> transferir(@RequestBody TransferenciaEntity transferenciaEntity) {
         logger.info("Class: TransferenciaController - Method: transferir");
         return ResponseEntity.ok(transferenciaService.transferir(transferenciaEntity));
     }
 
+    /**
+     * Endpoint para obter a lista de transferências financeiras agendadas para datas futuras.
+     *
+     * @return ResponseEntity contendo uma lista das transferências agendadas.
+     */
     @GetMapping("/agendamentos")
     public ResponseEntity<List<TransferenciaEntity>> agendamentos() {
-        logger.info("Class: TransferenciaController - Method: transferir");
+        logger.info("Class: TransferenciaController - Method: agendamentos");
         return ResponseEntity.ok(transferenciaService.getAgendamentos());
     }
 
+    /**
+     * Endpoint para obter a lista de transferências financeiras já realizadas.
+     *
+     * @return ResponseEntity contendo uma lista das transferências realizadas até a data atual.
+     */
     @GetMapping("/transferencias")
     public ResponseEntity<List<TransferenciaEntity>> transferencias() {
-        logger.info("Class: TransferenciaController - Method: transferir");
+        logger.info("Class: TransferenciaController - Method: transferencias");
         return ResponseEntity.ok(transferenciaService.getTransferencias());
     }
-
 }
